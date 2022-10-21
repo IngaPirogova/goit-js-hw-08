205b8aee361f4f04c734d7b0863f557ed32ab8bb
IngaPirogova <ingeborga.pirogova@gmail.com>
2022-10-21 22:11:19 +0300

    Update 03-js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,27 +1,26 @@
 import throttle from "lodash.throttle";
 
-const STORAGE_KEY = "feedback-form-state";
-
 const refs = {
     form: document.querySelector('.feedback-form'),
     textarea: document.querySelector('.feedback-form textarea'),
     input: document.querySelector('.feedback-form input'),
 };
-     console.log(refs.form)
-    
+
+const STORAGE_KEY = "feedback-form-state";
+const formData = {};
+     
  refs.form.addEventListener('submit', onFormSubmit);
- refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+ refs.form.addEventListener('input', throttle(onTextareaInput, 500));
  
- 
-populateTextarea();
 
 function onTextareaInput(e) {
-    const message = e.target.value;
-    localStorage.setItem('STORAGE_KEY', JSON.stringify(message));  
-    
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem('STORAGE_KEY', JSON.stringify(formData));      
 }
 
 function onFormSubmit(e) {
+    console.log(JSON.parse(localStorage.getItem('STORAGE_KEY')));
+
     e.preventDefault();
 
     if (!formData[refs.input.name] || !formData[refs.textarea.name]) {
@@ -30,21 +29,18 @@ function onFormSubmit(e) {
 
     console.log('Отправка формы')
     e.target.reset();
-    localStorage.removeItem('STORAGE_KEY');
+    localStorage.removeItem('STORAGE_KEY');    
 }
 
 function populateTextarea() {
-    const savedMessage = localStorage.getItem('STORAGE_KEY');
-        
+    const savedMessage = JSON.parse(localStorage.getItem('STORAGE_KEY'));
+    
     if (savedMessage) {
-        console.log(savedMessage);
-        refs.textarea.value = savedMessage;
+        
+        refs.textarea.value = savedMessage.message;
+        refs.input.value = savedMessage.email;
     }
 };
+populateTextarea();
 
-const formData = {};
-refs.form.addEventListener('input', e => {
-    formData[e.target.name] = e.target.value;
-    
-    console.log(formData);       
-});
+
