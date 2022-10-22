639f747a6e2648f5a877a7fc5d8c0a76992d173c
IngaPirogova <ingeborga.pirogova@gmail.com>
2022-10-22 12:29:34 +0300

    Update 03-js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,47 +1,51 @@
 import throttle from "lodash.throttle";
 
+const STORAGE_KEY = "feedback-form-state";
+const formData = {};
 const refs = {
     form: document.querySelector('.feedback-form'),
     textarea: document.querySelector('.feedback-form textarea'),
     input: document.querySelector('.feedback-form input'),
 };
 
-const STORAGE_KEY = "feedback-form-state";
-const formData = {};
      
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onTextareaInput, 500));
- 
+ populateTextarea();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    if (!formData[refs.input.name] || !formData[refs.textarea.name]) {
+         alert('Введите текст')
+    } 
+    console.log(formData);
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);  
+    formData[refs.input.name] = '';
+    formData[refs.textarea.name] = '';
+    
+}
 
 function onTextareaInput(e) {
     formData[e.target.name] = e.target.value;
-    localStorage.setItem('STORAGE_KEY', JSON.stringify(formData));      
-}
-
-function onFormSubmit(e) {
-    console.log(JSON.parse(localStorage.getItem('STORAGE_KEY')));
-
-    e.preventDefault();
-
-    if (!formData[refs.input.name] || !formData[refs.textarea.name]) {
-         alert('Введите текст')
-    }
-
-    console.log('Отправка формы')
-    e.target.reset();
-    localStorage.removeItem('STORAGE_KEY');    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));      
 }
 
 function populateTextarea() {
-    const savedMessage = JSON.parse(localStorage.getItem('STORAGE_KEY'));
-    
-    if (savedMessage) {
-        
-        refs.textarea.value = savedMessage.message;
-        refs.input.value = savedMessage.email;
+    const savedMessage = localStorage.getItem("feedback-form-state");
+    const parsedMessage = JSON.parse(savedMessage);
+        if (savedMessage) {        
+            if (parsedMessage.email) {
+                refs.input.value = parsedMessage.email;
+                formData[refs.input.name] = parsedMessage.email;
+
+            }
+            if (parsedMessage.message) {
+                refs.textarea.value = parsedMessage.message;
+                formData[refs.textarea.name] = parsedMessage.message;
+            }
     }
 };
 
-populateTextarea();
 
 
