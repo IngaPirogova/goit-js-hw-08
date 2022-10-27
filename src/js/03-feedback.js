da29468c9e414eb3e79c4d1b457029dda015f71e
IngaPirogova <ingeborga.pirogova@gmail.com>
2022-10-27 22:17:03 +0300

    upd

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -32,7 +32,7 @@ function onTextareaInput(e) {
 
 
 function populateTextarea() {
-    const savedMessage = localStorage.getItem("feedback-form-state");
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
     const parsedMessage = JSON.parse(savedMessage);
         if (savedMessage) {        
             if (parsedMessage.email) {
@@ -50,3 +50,8 @@ populateTextarea();
 
 
 
+// if (parsedMessage) {
+//       formData = parsedMessage;
+//       refs.input.value = formData.email || '';
+//       refs.textarea.value = formData.message || '';
+//     }
